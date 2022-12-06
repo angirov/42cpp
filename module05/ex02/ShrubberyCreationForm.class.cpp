f50ce1b9bd7ad78125a7715ac782e96bbc104a58
Vladimir Angirov <angirov@posteo.net>
2022-12-06 15:16:57 +0100

    m05 ex02 done?

---
+++ module05/ex02/ShrubberyCreationForm.class.cpp
@@ -0,0 +1,40 @@
+#include "ShrubberyCreationForm.class.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string target) : 
+	AForm("ShrubberyCreationForm", 25, 5), target(target) {
+	std::cout << ">>> ShrubberyCreationForm constructor is called." << std::endl;
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm() {
+	std::cout << ">>> ShrubberyCreationForm destructor is called." << std::endl;
+}
+
+std::ofstream & ShrubberyCreationForm::tree(std::ofstream & f) const {
+	f << "       ###" << std::endl;
+	f << "      #o###" << std::endl;
+	f << "    #####o###" << std::endl;
+	f << "   #o#=#|#=###" << std::endl;
+	f << "    ###=|=#o#" << std::endl;
+	f << "     # }|{  #" << std::endl;
+	f << "       }|{" << std::endl;
+	f << std::endl;
+	return f;
+}
+
+bool ShrubberyCreationForm::execAction() const {
+	std::string filename = target + "_shrubbery";
+	std::ofstream myfile;
+	myfile.open(filename.c_str());
+	if (myfile.is_open())
+	{
+		for (int i = 0; i < 3; i++)
+			this->tree(myfile);
+		myfile.close();
+		return true;
+	}
+	else
+	{
+		std::cerr << "### File could not be open" << std::endl;
+	return false;
+	}
+}
--- module05/ex02/main.cpp
+++ module05/ex02/main.cpp
@@ -1,6 +1,10 @@
 #include "Bureaucrat.class.hpp"
 #include "AForm.class.hpp"
 #include "PresidentialPardonForm.class.hpp"
+#include "RobotomyRequestForm.class.hpp"
+#include "ShrubberyCreationForm.class.hpp"
+
+#include <stdlib.h>
 
 int main()
 {
@@ -10,13 +14,35 @@ int main()
 	AForm * f = new PresidentialPardonForm("D4");
 	std::cout << *f << " exits now!" << std::endl;
 	e->signForm(*f);
-
+	e->executeForm(*f);
 	v->executeForm(*f);
+
+	AForm * r =  new RobotomyRequestForm("R2");
+	e->signForm(*r);
+	for (int i = 0; i < 10; i++)
+	{
+		e->executeForm(*r);
+	}
+
+	AForm * s =  new ShrubberyCreationForm("S12");
+	e->signForm(*s);
+	e->executeForm(*s);
 
 	delete e;
 	delete f;
+	delete r;
+	delete s;
 	return 0;
 }
+
+// int main()
+// {
+// 	for (int i=0; i < 10; i++)
+// 	{
+// 		std::cout << rand() % 2 << std::endl;
+// 	}
+// 	return 0;
+// }
 
 
 // https://www.youtube.com/watch?v=0ojB8c0xUd8
