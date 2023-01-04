5ebacd4fc860f41cfbe2d60c7b022a185ec174dc
Vladimir Angirov <angirov@posteo.net>
2023-01-04 11:36:07 +0100

    m00 ex00 ready

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -3,20 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vangirov <vangirov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vangirov <vangirov@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 11:49:57 by vangirov          #+#    #+#             */
-/*   Updated: 2022/10/05 10:54:38 by vangirov         ###   ########.fr       */
+/*   Updated: 2023/01/04 11:33:12 by vangirov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
+
+char* str2upper(char* s)
+{
+	for (int i = 0; s[i]; i++)
+			s[i] = toupper(s[i]);
+	return s; 
+}
 
 int main(int argc, char **argv)
 {
 	for (int i = 1; argv[i]; i++)
-		for (int j = 0; argv[i][j]; j++)
-			std::cout << (char)toupper(argv[i][j]);
+	{
+		std::cout << str2upper(argv[i]);
+	}
 	if (argc < 2)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	std::cout << std::endl;
