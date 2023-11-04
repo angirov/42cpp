b33f89b55b34e2a1c87bec9f00e991586ba69384
Vladimir Angirov <angirov@posteo.net>
2023-11-04 16:36:11 +0100

    m07-00 done

--- module07/ex00/Templates.hpp
+++ module07/ex00/Templates.hpp
@@ -3,7 +3,7 @@
 
 template<typename T>
 void swap(T &a, T &b) {
-	T temp {a};
+	T temp = a;
 	a = b;
 	b = temp;
 }
--- module07/ex00/main.cpp
+++ module07/ex00/main.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
 #include "Templates.hpp"
 
-int main() {
-	int a = 1;
-	int b = 2;
-	std::cout << "a = " << a << std::endl;
-	std::cout << "b = " << b << std::endl;
+class Awesome
+{
+  public:
+    Awesome(void) : _n(0) {}
+    Awesome( int n ) : _n( n ) {}
+    Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+    bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+    bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+    bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+    bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+    bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+    bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+    int get_n() const { return _n; }
+  private:
+    int _n;
+};
+std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
 
-	std::cout << ">>> swap is called" << std::endl;
-	swap(a, b);
+int main(void)
+{
+	Awesome a(2), b(4);
 
-	std::cout << "a = " << a << std::endl;
-	std::cout << "b = " << b << std::endl;
-	std::cout << "min(a, b) = " << min(a,b) <<std::endl;
-	std::cout << "max(a, b) = " << max(a,b) <<std::endl;
+        swap(a, b);
+        std::cout << a << " " << b << std::endl;
+        std::cout << max(a, b) << std::endl;
+        std::cout << min(a, b) << std::endl;
+        return (0);
 }
+
+// int main() {
+// 	int a = 1;
+// 	int b = 2;
+// 	std::cout << "a = " << a << std::endl;
+// 	std::cout << "b = " << b << std::endl;
+
+// 	std::cout << ">>> swap is called" << std::endl;
+// 	swap(a, b);
+
+// 	std::cout << "a = " << a << std::endl;
+// 	std::cout << "b = " << b << std::endl;
+// 	std::cout << "min(a, b) = " << min(a,b) <<std::endl;
+// 	std::cout << "max(a, b) = " << max(a,b) <<std::endl;
+// }
