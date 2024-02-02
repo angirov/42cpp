80b5bee7c66afafb282cfc6959abb4e191193368
Vladimir Angirov <angirov@posteo.net>
2024-02-02 01:53:37 +0100

    ready 9-2

--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -29,6 +29,7 @@ int main(void)
     }
     std::cout << std::endl;
 
+    std::cout << "==== Extra Test ====" << std::endl;
     MutantStack<int> s(mstack);
     MutantStack<int>::iterator it1;
     for (it1 = s.begin(); it1 != s.end(); ++it1)
@@ -39,3 +40,49 @@ int main(void)
 
     return 0;
 }
+
+
+// ==========================================
+// #include <iostream>
+// #include "MutantStack.hpp"
+// #include <list>
+
+// int main(void) 
+// {
+//     std::list<int> mstack;
+//     std::cout << "Pushed 5, 17" << std::endl;
+//     mstack.push_back(5);
+//     mstack.push_back(17);
+//     std::cout << "Top: " << mstack.back() << std::endl;
+//     mstack.pop_back();
+//     std::cout << "Poped, so left size: " << mstack.size() << std::endl;
+
+//     std::cout << "Pushed 3, 5, 737, 0" << std::endl;
+//     mstack.push_back(3);
+//     mstack.push_back(5);
+//     mstack.push_back(737);
+//     mstack.push_back(0);
+
+//     std::list<int>::iterator it = mstack.begin();
+//     std::list<int>::iterator ite = mstack.end();
+//     ++it;
+//     --it;
+//     std::cout << "Print out: ";
+//     while (it != ite)
+//     {
+//         std::cout << *it << " ";
+//         ++it;
+//     }
+//     std::cout << std::endl;
+
+//     std::cout << "==== Extra Test ====" << std::endl;
+//     std::list<int> s(mstack);
+//     std::list<int>::iterator it1;
+//     for (it1 = s.begin(); it1 != s.end(); ++it1)
+//     {
+//         std::cout << *it1 << " ";
+//     }
+//     std::cout << std::endl;
+
+//     return 0;
+// }
