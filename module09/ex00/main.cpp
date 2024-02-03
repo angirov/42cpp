be4f291531b16f92861cc9fe9a57e7aaa898eae1
Vladimir Angirov <angirov@posteo.net>
2024-02-03 10:55:08 +0100

    csv loader done

--- module09/ex00/main.cpp
+++ module09/ex00/main.cpp
@@ -25,8 +25,12 @@ void print_tm(struct tm &tm)
 
 class Btc
 {
+// private:
+    static const bool debug = DEBUG;
+    std::map<std::string, float> db;
+
 public:
-    void handle_csv(char *filepath)
+    void _load_csv(char *filepath)
     {
         std::string line;
         std::ifstream infile(filepath);
@@ -34,12 +38,46 @@ public:
         std::getline(infile, line); // skip the header
         while (std::getline(infile, line))
         {
-            this->handle_data_line(line, ',');
+            this->_load_line(line, ',');
         }
-        std::cout << " ====== Finished initing DB ======" << std::endl;
+        if (debug) std::cout << " ====== Finished initing DB ======" << std::endl;
     }
 
-    void handle_input(char *filepath)
+    void _load_line(std::string &line, char delim)
+    {
+        std::string segment, date;
+        std::stringstream ss(line);
+
+        std::getline(ss, segment, delim);
+        if (debug) std::cout << "DEBUG: loading data: date: " << segment;
+        if (!__valid_date(segment)) {
+            std::cerr << std::endl << 
+            "ERROR: loading data: invalid date: " << segment << std::endl;
+            exit(10);
+        }
+        date = segment;
+
+        std::getline(ss, segment, ',');
+        if (!__isNumeric(segment)) {
+            std::cerr << std::endl << 
+            "ERROR: loading data: invalid price: " << segment << std::endl;
+            exit(10);
+        }
+        float price = handle_float(segment);
+        if (debug) std::cout << " set to: " << price << std::endl;
+
+        
+        if (std::getline(ss, segment, ',')) {
+            std::cerr << std::endl << 
+            "ERROR: loading data: more than two columns: what is this?:" << segment << std::endl;
+            exit(10);
+        }
+
+        db.insert(std::pair<std::string, float>(date, price));
+    }
+
+
+    void process_input(char *filepath)
     {
         std::string line;
         std::ifstream infile(filepath);
@@ -49,18 +87,6 @@ public:
         {
             this->handle_input_line(line, '|');
         }
-    }
-    void iter_db()
-    {
-        std::map<std::string, float>::iterator it;
-        for (it = db.end(); it != db.begin(); --it)
-        {
-            std::cout << it->first << std::endl;
-        }
-    }
-    std::string earliest()
-    {
-        return db.begin()->first;
     }
 
     float find_price(std::string valid_date)
@@ -76,44 +102,11 @@ public:
             return std::numeric_limits<float>::quiet_NaN();
         }
     }
-    // private:
-    static const bool debug = DEBUG;
-    std::map<std::string, float> db;
-    std::string handle_date(std::string &str)
-    {
-        std::string date(str);
-        char buf[100];
-        struct tm tm;
-        // std::string result;
-
-        strptime(date.c_str(), TF, &tm);
-        strftime(buf, sizeof(buf), TF, &tm);
-
-        return buf;
-    }
 
     float handle_float(std::string &str)
     {
         float price = std::atof(str.c_str());
         return price;
-    }
-
-    void handle_data_line(std::string &line, char delim)
-    {
-        std::string segment;
-
-        std::stringstream ss(line);
-
-        std::getline(ss, segment, delim);
-        std::string date = handle_date(segment);
-        if (debug)
-            std::cout << "date: " << date;
-
-        std::getline(ss, segment, ',');
-        float price = handle_float(segment);
-        if (debug)
-            std::cout << " | set to: " << price << std::endl;
-        db[date] = price; ///////////////////////////////////////////////// insert
     }
 
     std::list<std::string> split_string(std::string const line, char delim)
@@ -136,8 +129,10 @@ public:
         std::stringstream ss(line);
 
         std::getline(ss, segment, delim);
-        std::string date = handle_date(segment);
-        std::cout << "date: " << date;
+        if (!__valid_date(segment))
+            std::cerr << "ERROR: processing input: invalid date: " << segment;
+        std::string date = segment;
+        std::cout << "date: " << segment;
         float price = find_price(date);
         std::cout << " => $" << price << "$\t"; // << std::endl;
 
@@ -158,19 +153,38 @@ public:
         return result;
     }
 
-    bool valid_date(std::string date) {
+    bool __valid_date(std::string date)
+    {
         struct tm tm;
-        if (strptime((date).c_str(), TF, &tm)){
-            std::cout << "Valid date: " << date << std::endl;
+        if (strptime((date).c_str(), TF, &tm))
+        {
             return true;
-        } else {
-            std::cout << "Invalid date: " << date << std::endl;
+        }
+        else
+        {
             return false;
         }
     }
+
+    bool __isNumeric(std::string str) {
+        int len = str.length();
+        char c;
+
+        for (int i = 0; i < len; i++) {
+            c = str[i];
+            if (i == 0 && (c == '-' || c == '+'))
+                continue ;
+            else if (i == len-1 && c == 'f')
+                continue ;
+            else if (isdigit(c) || c == '.')
+                continue ;
+            else
+                return (false);
+        }
+        return (true);
+    }
+
 };
-
-
 
 int main(int argc, char **argv)
 {
@@ -180,8 +194,8 @@ int main(int argc, char **argv)
     char *input_path = argv[2];
     std::cout << csv_path << input_path << std::endl;
     Btc btc;
-    btc.handle_csv(csv_path);
-    btc.handle_input(input_path);
+    btc._load_csv(csv_path);
+    btc.process_input(input_path);
     // btc.iter_db();
     std::cout << btc.find_price("2015-05-02") << std::endl;
     std::cout << btc.find_price("3333-05-05") << std::endl;
@@ -194,10 +208,37 @@ int main(int argc, char **argv)
         // int part;
     }
 
-    btc.valid_date("2022-12-02");
-    btc.valid_date("2222-12-02");
-    btc.valid_date("2022-13-02");
-    btc.valid_date("2022-13-42");
+    btc.__valid_date("2022-12-02");
+    btc.__valid_date("2222-12-02");
+    btc.__valid_date("2022-13-02");
+    btc.__valid_date("2022-13-42");
     // std::string s("2015-01-03");
     // std::cout << next_day(s, -1) << std::endl;
 }
+
+// void iter_db()
+// {
+//     std::map<std::string, float>::iterator it;
+//     for (it = db.end(); it != db.begin(); --it)
+//     {
+//         std::cout << it->first << std::endl;
+//     }
+// }
+// std::string earliest()
+// {
+//     return db.begin()->first;
+// }
+// std::string handle_date(std::string &str)
+// {
+//     std::string date(str);
+//     char buf[100];
+//     struct tm tm;
+//     // std::string result;
+
+//     strptime(date.c_str(), TF, &tm);
+//     strftime(buf, sizeof(buf), TF, &tm);
+
+//     return buf;
+// }
+
+
