f14db91b97218e5c5abe9b0c0b39461055d78c26
Vladimir Angirov <angirov@posteo.net>
2024-02-05 11:31:21 +0100

    ins-merge for list seem to work

--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -1,73 +1,161 @@
+#define DEGUG false
+
 #include <iostream>
 #include <vector>
+#include <list>
+#include <limits>
+#include <stdlib.h>
+#include <ctime>
+#define K 14
 
-#define K 3
-
-void insertionSort(std::vector<int>& A, int start, int finish) {
+void insertionSort(std::vector<int>& vec, int start, int finish) {
     for (int i = start; i < finish; i++) {
-        int tempVal = A[i + 1];
+        int tempVal = vec[i + 1];
         int j = i + 1;
-        while (j > start && A[j - 1] > tempVal) {
-            A[j] = A[j - 1];
+        while (j > start && vec[j - 1] > tempVal) {
+            vec[j] = vec[j - 1];
             j--;
         }
-        A[j] = tempVal;
+        vec[j] = tempVal;
     }
+
+    // for debugging only
     for (int i=start; i <= finish; ++i) {
-        std::cout << ">> [" << i << "] " << A[i] << std::endl;
+        if (DEGUG) std::cout << ">> [" << i << "] " << vec[i] << std::endl;
     }
-    std::cout << "---------" << std::endl;
+    if (DEGUG) std::cout << "---------" << std::endl;
 }
 
-void merge(std::vector<int>& A, int s, int m, int e) {
-    int n1 = m - s + 1;
-    int n2 = e - m;
-    std::vector<int> LA(A.begin() + s, A.begin() + m + 1);
-    std::vector<int> RA(A.begin() + m + 1, A.begin() + e + 1);
-    int RIDX = 0;
-    int LIDX = 0;
-    for (int i = s; i < e - s + 1; i++) {
-        if (RIDX == n2) {
-            A[i] = LA[LIDX];
-            LIDX++;
-        } else if (LIDX == n1) {
-            A[i] = RA[RIDX];
-            RIDX++;
-        } else if (RA[RIDX] > LA[LIDX]) {
-            A[i] = LA[LIDX];
-            LIDX++;
+void merge(std::vector<int>& vec, int s, int m, int e) {
+    int lsize = m - s + 1;
+    int rsize = e - m;
+    std::vector<int> lpart(vec.begin() + s, vec.begin() + m + 1);
+    std::vector<int> rpart(vec.begin() + m + 1, vec.begin() + e + 1);
+    int li = 0;
+    int ri = 0;
+    int k = s;
+
+    while (li < lsize && ri < rsize) {
+        if (lpart[li] <= rpart[ri])
+            vec[k++] = lpart[li++];
+        else
+            vec[k++] = rpart[ri++];
+    }
+
+    while (li < lsize)
+        vec[k++] = lpart[li++];
+
+    while (ri < rsize)
+        vec[k++] = rpart[ri++];
+}
+
+void sort(std::vector<int>& vec, int start, int finish) {
+    int Smiddle = (start + finish) / 2;
+    if (DEGUG) std::cout << "start:" << start << " finish:" << finish << std::endl;
+    if (finish - start >= K) {
+        sort(vec, start, Smiddle);
+        sort(vec, Smiddle + 1, finish);
+        merge(vec, start, Smiddle, finish);
+    } else {
+        insertionSort(vec, start, finish);
+    }
+}
+
+bool is_int_literal(std::string str)
+{
+    int len = str.length();
+    char c;
+
+    for (int i = 0; i < len; i++)
+    {
+        c = str[i];
+        if (isdigit(c))
+            continue;
+        else
+            return (false);
+    }
+    return (true);
+}
+void exit_msg(std::string msg, int n) {
+    std::cerr << "\nPmergeMe: " << msg << std::endl;
+    exit(n);
+}
+
+bool is_int(long n) {
+    int imin = std::numeric_limits<int>::min();
+    int imax = std::numeric_limits<int>::max();
+
+    return (n <= imax && n >= imin);
+}
+
+
+std::string::iterator skip_whitespace(std::string::iterator &start, std::string::iterator &end) {
+    while (std::isspace(*start) && start != end) {
+    start++;
+    }
+    return start;
+}
+
+std::string get_next_word(std::string::iterator &it, std::string::iterator &end) {
+    skip_whitespace(it, end);
+    std::string::iterator start = it;
+    while (!std::isspace(*it) && it != end) {
+        it++;
+    }
+    std::string result = std::string(start, it);
+    return result;
+};
+
+std::list<int> process_input(int argc, char **argv) {
+    std::list<int> collector;
+
+    for (int i=1; i<argc; ++i) {
+        std::string word(argv[i]);
+        if (is_int_literal(word)) {
+            long n = atol(word.c_str());
+            if (n == 0 && word != "0")
+                exit_msg("Bad input: Only ingeter numbers allowed: " + word, 1);
+            if (n < 0)
+                exit_msg("Bad input: Only positive numbers allowed: " + word, 1);
+            if (!is_int(n))
+                exit_msg("One of the input numbers is beyond integer limits: " + word, 6);
+            collector.push_back(n);
         } else {
-            A[i] = RA[RIDX];
-            RIDX++;
+            exit_msg("Bad input: " + word, 20);
         }
     }
+    return collector;
 }
 
-void sort(std::vector<int>& A, int Sstart, int Sfinish) {
-    int Smiddle = (Sstart + Sfinish) / 2;
-    std::cout << "Sstart:" << Sstart << " Sfinish:" << Sfinish << std::endl;
-    if (Sfinish - Sstart > K) {
-        sort(A, Sstart, Smiddle);
-        sort(A, Smiddle + 1, Sfinish);
-        merge(A, Sstart, Smiddle, Sfinish);
-    } else {
-        insertionSort(A, Sstart, Sfinish);
+int main(int argc, char** argv) {
+    if (argc < 2)
+        exit_msg("ERROR: wrong number of arguments.", 10);
+    std::list<int> lst = process_input(argc, argv);
+    for (std::list<int>::iterator it = lst.begin(); it != lst.end(); ++it) {
+        if (DEGUG) std::cout << *it << " ";
     }
-}
-
-int main() {
-    std::vector<int> A = { 2, 5, 1, 6, 7, 3, 8, 4, 9 };
+    if (DEGUG) std::cout << std::endl;
+    std::vector<int> vec(lst.begin(), lst.end());
     std::cout << "Before: ";
-    for (int i=0; i < A.size(); ++i) {
-        std::cout << A[i] << ", ";
+    for (int i=0; (unsigned)i < vec.size(); ++i) {
+        std::cout << vec[i] << ", ";
     }
     std::cout << std::endl;
 
-    sort(A, 0, A.size() - 1);
+    std::clock_t start_vec = std::clock();
+    sort(vec, 0, vec.size() - 1);
+    std::clock_t stop_vec = std::clock();
 
+    std::cout << "\n============================================== " << std::endl;
     std::cout << "After: ";
-    for (int i=0; i < A.size(); ++i) {
-        std::cout << A[i] << ", ";
+    int prev = -1;
+    for (int i=0; (unsigned)i < vec.size(); ++i) {
+        if (vec[i] < prev)
+            exit_msg("Sorting failed: ", 42);
+        std::cout << vec[i] << ", ";
+        prev = vec[i];
     }
     std::cout << std::endl;
+
+    std::cout << ((stop_vec - start_vec) * 100000 / CLOCKS_PER_SEC) << "us" << std::endl;
 }
--- module09/ex02/main_list.cpp
+++ module09/ex02/main_list.cpp
@@ -1,7 +1,38 @@
+#define DEGUG false
 #include <iostream>
 #include <list>
+#include <limits>
+#include <ctime>
 
-#define K 3
+#define K 2
+void exit_msg(std::string msg, int n) {
+    std::cerr << "\nPmergeMe: " << msg << std::endl;
+    exit(n);
+}
+
+bool is_int(long n) {
+    int imin = std::numeric_limits<int>::min();
+    int imax = std::numeric_limits<int>::max();
+
+    return (n <= imax && n >= imin);
+}
+
+
+bool is_int_literal(std::string str)
+{
+    int len = str.length();
+    char c;
+
+    for (int i = 0; i < len; i++)
+    {
+        c = str[i];
+        if (isdigit(c))
+            continue;
+        else
+            return (false);
+    }
+    return (true);
+}
 
 void insertionSort(std::list<int>& A, int start, int finish) {
     auto itStart = std::next(A.begin(), start);
@@ -24,8 +55,8 @@ void insertionSort(std::list<int>& A, int start, int finish) {
 }
 
 void merge(std::list<int>& A, int s, int m, int e) {
-    int n1 = m - s + 1;
-    int n2 = e - m;
+    // int n1 = m - s + 1;
+    // int n2 = e - m;
 
     std::list<int> LA(std::next(A.begin(), s), std::next(A.begin(), m + 1));
     std::list<int> RA(std::next(A.begin(), m + 1), std::next(A.begin(), e + 1));
@@ -53,31 +84,96 @@ void merge(std::list<int>& A, int s, int m, int e) {
     }
 }
 
-void sort(std::list<int>& A, int Sstart, int Sfinish) {
+void sort_list(std::list<int>& A, int Sstart, int Sfinish) {
     int Smiddle = (Sstart + Sfinish) / 2;
     std::cout << "Sstart:" << Sstart << " Sfinish:" << Sfinish << std::endl;
     if (Sfinish - Sstart > K) {
-        sort(A, Sstart, Smiddle);
-        sort(A, Smiddle + 1, Sfinish);
+        sort_list(A, Sstart, Smiddle);
+        sort_list(A, Smiddle + 1, Sfinish);
         merge(A, Sstart, Smiddle, Sfinish);
     } else {
         insertionSort(A, Sstart, Sfinish);
     }
 }
 
-int main() {
-    std::list<int> A = {2, 5, 1, 6, 7, 3, 8, 4, 9};
+// int main() {
+//     std::list<int> A = {2, 5, 1, 6, 7, 3, 8, 4, 9};
+//     std::cout << "Before: ";
+//     for (const auto& element : A) {
+//         std::cout << element << ", ";
+//     }
+//     std::cout << std::endl;
+
+//     sort_list(A, 0, A.size() - 1);
+
+//     std::cout << "After: ";
+//     for (const auto& element : A) {
+//         std::cout << element << ", ";
+//     }
+//     std::cout << std::endl;
+// }
+
+std::list<int> process_input(int argc, char **argv) {
+    std::list<int> collector;
+
+    for (int i=1; i<argc; ++i) {
+        std::string word(argv[i]);
+        if (is_int_literal(word)) {
+            long n = atol(word.c_str());
+            if (n == 0 && word != "0")
+                exit_msg("Bad input: Only ingeter numbers allowed: " + word, 1);
+            if (n < 0)
+                exit_msg("Bad input: Only positive numbers allowed: " + word, 1);
+            if (!is_int(n))
+                exit_msg("One of the input numbers is beyond integer limits: " + word, 6);
+            collector.push_back(n);
+        } else {
+            exit_msg("Bad input: " + word, 20);
+        }
+    }
+    return collector;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2)
+        exit_msg("ERROR: wrong number of arguments.", 10);
+    std::list<int> lst = process_input(argc, argv);
+    for (std::list<int>::iterator it = lst.begin(); it != lst.end(); ++it) {
+        if (DEGUG) std::cout << *it << " ";
+    }
+    if (DEGUG) std::cout << std::endl;
+    // std::vector<int> vec(lst.begin(), lst.end());
+    // std::cout << "Before: ";
+    // for (int i=0; (unsigned)i < lst.size(); ++i) {
+    //     std::cout << vec[i] << ", ";
+    // }
+    // std::cout << std::endl;
     std::cout << "Before: ";
-    for (const auto& element : A) {
-        std::cout << element << ", ";
+    for (std::list<int>::iterator it = lst.begin(); it != lst.end(); ++it) {
+        std::cout << *it << ", ";
     }
     std::cout << std::endl;
 
-    sort(A, 0, A.size() - 1);
+    std::clock_t start_vec = std::clock();
+    sort_list(lst, 0, lst.size() - 1);
+    std::clock_t stop_vec = std::clock();
 
+    std::cout << "\n============================================== " << std::endl;
     std::cout << "After: ";
-    for (const auto& element : A) {
-        std::cout << element << ", ";
+    int prev = -1;
+    // for (int i=0; (unsigned)i < lst.size(); ++i) {
+    //     if (vec[i] < prev)
+    //         exit_msg("Sorting failed: ", 42);
+    //     std::cout << vec[i] << ", ";
+    //     prev = vec[i];
+    // }
+    // std::cout << std::endl;
+    for (std::list<int>::iterator it = lst.begin(); it != lst.end(); ++it) {
+        std::cout << *it << ", ";
+        if (*it < prev)
+            exit_msg("Sorting failed: ", 42);
     }
     std::cout << std::endl;
+
+    std::cout << ((stop_vec - start_vec) * 100000 / CLOCKS_PER_SEC) << "us" << std::endl;
 }
